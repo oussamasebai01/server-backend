44f398f0ed79798d0742119f95b70ffd2a73ac7a
Oussama <oussama.sebai@esprit.tn>
2024-11-13 14:45:17 +0100

    crossOrigine

--- src/main/java/com/nadherarroum/server/resource/ServerResource.java
+++ src/main/java/com/nadherarroum/server/resource/ServerResource.java
@@ -27,6 +27,7 @@ import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 @RestController
 @RequestMapping("/server")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ServerResource {
 
     private final ServerServiceImpl serverService;
