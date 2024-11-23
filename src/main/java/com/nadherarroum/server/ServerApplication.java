99641a70eb2a3b62d9bf4552fbae23f08e9a9cfa
Oussama <74916176+oussamasebai01@users.noreply.github.com>
2024-11-23 21:52:13 +0100

    Update ServerApplication.java

--- src/main/java/com/nadherarroum/server/ServerApplication.java
+++ src/main/java/com/nadherarroum/server/ServerApplication.java
@@ -78,20 +78,20 @@ public class ServerApplication {
 //		return bean;
 //	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+	@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://192.168.100.104:4000"); // Origine autorisée
+        config.addAllowedHeader("*"); // Autoriser tous les en-têtes
+        config.addAllowedMethod("*"); // Autoriser toutes les méthodes (GET, POST, etc.)
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Appliquer pour tous les endpoints
+        
+        return new CorsFilter(source);
+    }
+}
 
 }
